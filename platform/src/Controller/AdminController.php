d6dadc1a35cd008ec6547054e9642b88b0e3aba7
James Blanksby <james@thisismyengine.com>
2026-05-13 08:35:04 +0300

    w

--- platform/src/Controller/AdminController.php
+++ platform/src/Controller/AdminController.php
@@ -4,8 +4,6 @@ namespace Platform\Controller;
 
 use Framework\Controller\Controller;
 use Framework\Http\Response\Response;
-use Platform\Form\Form;
-use Platform\Form\FormFactory;
 
 abstract class AdminController extends Controller
 {
@@ -14,10 +12,5 @@ abstract class AdminController extends Controller
         // @TODO redirect
 
         return $this->json($data);
-    }
-
-    public function createForm(string $type, array $data = []): Form
-    {
-        return $this->container->get(FormFactory::class)->create($type, $data);
     }
 }
